71ebc56d9cfcf03375484a720d9a19cab1e259e1
Phil Neibaur <pneibaur@Phils-MacBook-Pro.local>
2022-10-20 16:09:08 -0600

    adds authorization functionality

--- vistas_collector/models.py
+++ vistas_collector/models.py
@@ -1,7 +1,6 @@
-from email.policy import default
-from random import choices
 from django.db import models
 from django.urls import reverse
+from django.contrib.auth.models import User
 
 # UPCOMING CATEGORY FEATURE
 # TYPES = ('Beach', 'Mountain', 'Land', 'Sky', 'Star', 'City', 'Rural', 'Desert', 'Lake', 'Ocean', 'Frozen',)
@@ -16,6 +15,7 @@ SEASONS = (
 
 class Flair(models.Model):
     tag = models.CharField(max_length=100)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
 
     def get_absolute_url(self):
         return reverse("flair_detail", kwargs={"pk": self.id})
@@ -35,6 +35,8 @@ class Vista(models.Model):
     state_province = models.CharField(max_length=100)
     country = models.CharField(max_length=100)
     tags = models.ManyToManyField(Flair)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
 
     def __str__(self):
         return self.name
@@ -50,6 +52,7 @@ class Comment(models.Model):
     comment = models.TextField(max_length=250)
     date = models.DateField(auto_now_add=True)
     vista = models.ForeignKey(Vista, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
 
     def __str__(self):
         return self.comment
--- vistas_collector/views.py
+++ vistas_collector/views.py
@@ -1,9 +1,14 @@
+from multiprocessing import context
 from django.shortcuts import render, redirect
 # from django.http import HttpResponse
-from vistas_collector.models import Flair, Vista, Comment
+from vistas_collector.models import Flair, Vista, Comment, User
 from django.views.generic import ListView, DetailView
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from .forms import CommentForm, FlairForm
+from django.contrib.auth import login
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.mixins import LoginRequiredMixin
 
 
 def home(request):
@@ -13,11 +18,17 @@ def home(request):
 def about(request):
     return render(request, 'about.html')
 
-
+@login_required
 def vistas_index(request):
     vistas = Vista.objects.all()
     return render(request, "vistas/index.html", {"vistas": vistas})
 
+@login_required
+def user_vistas(request):
+    vistas = request.user.vista_set.all()
+    return render(request, 'vistas/index.html', {'vistas': vistas})
+
+@login_required
 def vista_detail(request, vista_id):
     vista = Vista.objects.get(id=vista_id)
     comment_form = CommentForm()
@@ -30,65 +41,91 @@ def vista_detail(request, vista_id):
         'unassociated_tags': unassociated_tags
         })
 
-
-def add_comment(request, vista_id):
+@login_required
+def add_comment(request, vista_id, user_id):
     form = CommentForm(request.POST)
     if form.is_valid():
         new_comment = form.save(commit=False)
         new_comment.vista_id = vista_id
+        new_comment.user_id = user_id
         new_comment.save()
     return redirect("vista_detail", vista_id=vista_id)
 
+@login_required
 def remove_comment(request, vista_id, comment_id):
     Comment.objects.get(id=comment_id).delete()
     return redirect("vista_detail", vista_id=vista_id)
 
-def add_flair(request, vista_id):
+@login_required
+def add_flair(request, vista_id, user_id):
     form = FlairForm(request.POST)
     if form.is_valid():
         new_flair = form.save(commit=False)
         new_flair.vista_id = vista_id
+        new_flair.user_id = user_id
         v = Vista.objects.get(id=vista_id)
-        f = Flair.objects.create(tag=new_flair)
+        u = User.objects.get(id=user_id)
+        f = Flair.objects.create(tag=new_flair, user=u)
         v.tags.add(f)
         v.save()
     return redirect('vista_detail', vista_id=vista_id)
 
+@login_required
 def assign_flair(request, vista_id, flair_id):
     Vista.objects.get(id=vista_id).tags.add(flair_id)
     return redirect('vista_detail', vista_id=vista_id)
 
+@login_required
 def unassign_flair(request, vista_id, flair_id):
     Vista.objects.get(id=vista_id).tags.remove(flair_id)
     return redirect('vista_detail', vista_id=vista_id)
 
-class VistaCreate(CreateView):
+class VistaCreate(LoginRequiredMixin, CreateView):
     model = Vista
     fields = ["name", "img", "description", "season", "year", "location", "state_province", "country",]
     success_url = "/vistas/"
 
+    def form_valid(self, form):
+        form.instance.user = self.request.user
+        return super().form_valid(form)
 
-class VistaUpdate(UpdateView):
+
+class VistaUpdate(LoginRequiredMixin, UpdateView):
     model = Vista
     fields = ["name", "description", "season", "year", "location", "state_province", "country",]
 
 
-class VistaDelete(DeleteView):
+class VistaDelete(LoginRequiredMixin, DeleteView):
     model = Vista
     success_url = "/vistas/"
 
-class FlairList(ListView):
+class FlairList(LoginRequiredMixin, ListView):
     model = Flair
     template_name = 'flairs/index.html'
 
-class FlairDetail(DetailView):
+class FlairDetail(LoginRequiredMixin, DetailView):
     model = Flair
     template_name = 'flairs/detail.html'
 
-class FlairUpdate(UpdateView):
+class FlairUpdate(LoginRequiredMixin, UpdateView):
     model = Flair
     fields = ['tag',]
 
-class FlairDelete(DeleteView):
+class FlairDelete(LoginRequiredMixin, DeleteView):
     model = Flair
     success_url = '/flairs/'
+
+
+def signup(request):
+    error_message = ''
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect("index")
+        else:
+            error_message = "Invalid signup - please try again."
+    form = UserCreationForm()
+    context = {'form': form, 'error_message': error_message}
+    return render(request, 'registration/signup.html', context)
